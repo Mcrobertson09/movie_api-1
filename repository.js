70d3dd1b1bcf5c43b14b71d91d939c73b7b881c7
Mcrobertson09 <84151467+Mcrobertson09@users.noreply.github.com>
2022-10-19 15:45:22 -0700

    updated API end Points

--- repository.js
+++ repository.js
@@ -1,29 +1,34 @@
 let topMovies = [
     {
-      title: 'Bloodsport',
-      director: 'Newt Arnold',
-      year:"1988",
-      genre:"Action" 
+        id: '1',
+        title: 'Bloodsport',
+        director: 'Newt Arnold',
+        year:"1988",
+        genre:"Action" 
     },
     {
+        id: 2,
         title: 'Lord Of the Rings',
         director: 'Peter Jackson',
         year:"2001",
         genre:"Fantasy" 
     },
     {
+        id: 3,
         title: 'Creed',
         director: 'Ryan Coogler',
         year:"2015",
         genre:"Action" 
     },
     {
+        id: 4,
         title: 'Avengers: Infinity War',
         director: 'Anthony & Joe Russo',
         year:"2018",
         genre:"Action" 
     },
     {
+        id: 5,
         title: 'Black Panter',
         director: 'Ryan Coogler',
         year:"2018",
